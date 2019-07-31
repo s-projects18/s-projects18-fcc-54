880666537fb53237a4725ecfb02f8a77b0a5230c
Glitch (s-projects18-fcc-54) <none>
2019-07-31 08:32:56 +0000

    getUser first implementation

--- helper/database.js
+++ helper/database.js
@@ -59,9 +59,9 @@ exports.createExercise = (userId, exercise) => {
       if(user==null) {
         reject("no user found for: "+userId);
       } else {
-        user.exercises.push(exerciseObject);
+        user.exercises.push(exerciseObject); // modify user-object
 
-        const pr = user.save();     
+        const pr = user.save(); // save user-object    
         pr.then(doc=>{
           resolve(doc);  
         }).catch(err=>{
@@ -100,4 +100,63 @@ exports.getAllUser = () => {
       else resolve(docs);
     });    
   });
+}
+
+// get a list of one user (exercises are included)
+// this works but cleaning exercise is don per array-funcs
+// TODO: solution probably with db.orders.aggregate?
+exports.getUser = args => {
+  let query = {_id:args.userId};
+  
+  // 0: don't show these columns
+  let projection = {
+    __v:0,
+    insert_date:0
+  };
+  
+  return new Promise((resolve, reject)=>{
+    let queryObj = Users.findOne(query, projection);
+    // doc is a Mongoose-object that CAN'T be modified
+    // lean()+exec() will return a plain JS-object instead
+    // https://stackoverflow.com/questions/14504385/why-cant-you-modify-the-data-returned-by-a-mongoose-query-ex-findbyid
+    queryObj.lean().exec((err, doc)=>{
+      if(doc==null) {
+        reject("no user found");
+      } else {
+        // {"_id":"BkT0mzhGr","username":"moo","count":1,"log":[{"description":"ex1","duration":3,"date":"Mon Jul 29 2019"}]}
+        doc.log=[];
+        doc.exercises.forEach((v,i)=>{
+          let cp = Object.assign({}, v);
+          delete cp._id;
+          doc.log.push(cp);
+        });
+        doc.count=doc.exercises.length;
+        delete doc.exercises;
+        doc.log = cleanupLog(doc.log, args);
+        resolve(doc);
+      }
+    });
+  });    
+}
+
+// array-solution (without database)
+const cleanupLog = (log, args) => {
+  let c = JSON.parse(JSON.stringify(log));
+  c = log.filter((v,i)=>{
+    let d1t = new Date(v.date);
+    let d1 = new Date(d1t.getFullYear(), d1t.getMonth(), d1t.getDate()).getTime(); // set time  to 0
+    let d2;
+    
+    if(args.from) {
+      d2 = new Date(args.from).getTime();
+      if(d1<d2) return false;
+    }
+    if(args.to) {
+      d2 = new Date(args.to).getTime();
+      if(d1>d2) return false;
+    }    
+    return true;
+  });
+  if(args.limit) c = c.slice(0, args.limit);
+  return c;
 }
